89bbefd84a1eab9de4fdcd665cc125a0406369ba
petersudai <psudai@gmail.com>
2023-09-07 11:25:17 -0700

    hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - printf is used to print output on the screen
+ *
+ * Return: 0 when the program executes successfully
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
