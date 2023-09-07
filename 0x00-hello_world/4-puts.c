1e58a3736da1cca777bfb7e25441b70668099018
petersudai <psudai@gmail.com>
2023-09-07 05:46:09 -0700

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,24 @@
-#include <stdio.h>
+/*
+ * File:4-puts.c
+ * Author: [Peter Sudai]
+ * Year: [2023]
+ * Description: Hello, puts
+ */
 
+
+#include <stdio.h>
+/**
+ * main - Entry point of the program
+ *
+ * Return: Always 0 (Success)
+ */
 int main() {
-    puts("Programming is like building a multilingual puzzle");
+  /*
+	 * puts - Function to print a string to stdout
+	 * @str: The string to be printed
+	 *
+	 * Return: On success, returns a non-negative number, on error, EOF is returned.
+	 */ 
+       	puts("Programming is like building a multilingual puzzle");
     return 0;
 }
