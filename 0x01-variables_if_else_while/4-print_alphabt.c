4861cffee8718403cac423a01b721eaf4f4bc85c
petersudai <psudai@gmail.com>
2023-09-08 05:45:04 -0700

    Alphabet soup

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - program that prints the alphabet in lowercase, except q and e
+ *
+ * Return: 0 Always (success)
+ */
+
+int main(void)
+{
+	char i;
+
+	for (i = 'a' ; i <= 'z' ; i++)
+		if (i != 'q' && i != 'e')
+			putchar(i);
+	putchar('\n');
+	return (0);
+}
