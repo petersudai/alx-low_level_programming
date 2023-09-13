1077c058ade70e8d84d414904a2e0a5287e827d5
petersudai <psudai@gmail.com>
2023-09-13 11:20:45 -0700

    Fibonacci

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+
+/**
+ * generateFibonacci - Generates the first n Fibonacci numbers and prints them.
+ * @n: The number of Fibonacci numbers to generate.
+ *
+ * This function generates the first n Fibonacci numbers and prints them,
+ * separated by commas.
+ */
+void generateFibonacci(int n)
+{
+	long long int fib[n];
+	int i;
+
+	fib[0] = 1;
+	fib[1] = 2;
+
+	for (i = 2; i < n; i++)
+	{
+		fib[i] = fib[i - 1] + fib[i - 2];
+	}
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%lld", fib[i]);
+		if (i != n - 1)
+		{
+			printf(", ");
+		}
+	}
+	printf("\n");
+}
+
+/**
+ * main - Entry point of the program.
+ *
+ * Return: Always 0 (Success).
+ */
+int main(void)
+{
+	generateFibonacci(50);
+	return (0);
+}
