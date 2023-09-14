2ea42a90582a600ab8f611271f8d15ad62800835
petersudai <psudai@gmail.com>
2023-09-14 00:42:45 -0700

    Multiple Mains

--- 0x03-debugging/main.c
+++ 0x03-debugging/main.c
@@ -5,12 +5,10 @@
 * Return: 0
 */
 
-int main(void)
+void positive_or_negative(int i)
 {
-        int i;
-
-        i = 98;
-        positive_or_negative(i);
-
-        return (0);
+	int i;
+	i = 98;
+	positive_or_negative(i);
+	return (0);
 }
