dc315bf8e4f5870e69ac660c8928fdf05901c7b1
petersudai <psudai@gmail.com>
2023-09-20 23:56:25 -0700

    Expect the best. Prepare for the worst. Capitalize on what comes

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -7,7 +7,23 @@
  * Return: str
  */
 
-char *cap_string(char *)
+char *cap_string(char *str)
 {
+int capitalize = 1;
 
+while (*str != '\0')
+	if ((*str >= 'a'  && *str <= 'z') || (*str >= 'A' && *str <= 'Z'))
+	{
+		if (capitalize && (*str >= 'a' && *str <= 'z'))
+		{
+			*str = *str - 32;
+			capitalize = 0;
+		}
+		else if (!capitalize && (*str >= 'A' && *str <= 'Z'))
+			*str = *str + 32;
+		else
+			capitalize = 1;
+		str++;
+	}
+return (str);
 }
