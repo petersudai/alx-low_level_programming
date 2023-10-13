8fd9ab6b4aba63840040ab55f47189019d79f513
petersudai <psudai@gmail.com>
2023-10-13 07:21:17 -0700

    To be is a to be the value of a variable

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -3,7 +3,11 @@
 /**
  * print_all - prints anything
  * @format: list of types of arguments passed to the function
- *
+ * @...: arguments corresponding to the format specifiers
+ * c: char
+ * i: integer
+ * f: float
+ * s: char * (if the string is NULL, print (nil) instead*
  * Return: void
  */
 void print_all(const char * const format, ...)
@@ -13,11 +17,15 @@ void print_all(const char * const format, ...)
 	char *str;
 	float f;
 	int i;
+	int first_arg = 1;
 
 	va_start(args, format);
 
 	while (ptr != NULL && *ptr != '\0')
 	{
+		if (!first_arg)
+			printf(", ");
+
 		switch (*ptr)
 		{
 			case 'c':
@@ -39,13 +47,9 @@ void print_all(const char * const format, ...)
 			default:
 				break;
 		}
-		if (*(ptr + 1) && (*ptr == 'c' || *ptr == 'i' ||
-				*ptr == 'f' || *ptr == 's'))
-		{
-			char *sep = ", ";
-
-			printf("%s", sep);
-		}
+		if (*(ptr + 1) != '\0' && (*ptr == 'c' || *ptr == 'i' ||
+					*ptr == 'f' || *ptr == 's'))
+			printf(", ");
 		ptr++;
 	}
 	va_end(args), printf("\n");
