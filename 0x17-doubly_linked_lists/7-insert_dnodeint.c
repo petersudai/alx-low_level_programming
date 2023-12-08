611754ca530105a28882cf147b1ff6ad337aa413
petersudai <psudai@gmail.com>
2023-12-07 22:40:21 -0800

    Insert at index

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -35,7 +35,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	}
 
 	temp = *h;
-	while (temp != NULL && count < idx -1)
+	while (temp != NULL && count < idx - 1)
 	{
 		temp = temp->next;
 		count++;
