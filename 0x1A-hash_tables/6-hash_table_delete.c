19fbc79515ef2891708c5d6594a7cbfb0363d70e
petersudai <psudai@gmail.com>
2024-01-02 07:48:14 -0800

    >>> del ht

---
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -0,0 +1,31 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_delete - deletes a hash table
+ * @ht: hash table to be deleted
+ */
+
+void hash_table_delete(hash_table_t *ht)
+{
+	unsigned long int i;
+	hash_node_t *current_node, *next_node;
+
+	if (ht == NULL)
+		return;
+
+	for (i = 0; i < ht->size; i++)
+	{
+		current_node = ht->array[i];
+		while (current_node != NULL)
+		{
+			next_node = current_node->next;
+			free(current_node->key);
+			free(current_node->value);
+			free(current_node);
+			free(current_node);
+			current_node = next_node;
+		}
+	}
+	free(ht->array);
+	free(ht);
+}
